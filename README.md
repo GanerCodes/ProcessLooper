1181a242edbc3da6d4a0e847fa34ee7795d9cc3b
Ganer <34655998+GanerCodes@users.noreply.github.com>
2021-12-05 03:14:08 -0600

    d

---
+++ README.md
@@ -0,0 +1,31 @@
+`python3 main.py <config>`
+
+Name:
+    Name of task
+    
+    Default = default
+Cmd (required):
+    Command to be executed  
+Time:
+    Time to take in seconds.
+    
+    Default = 10  
+RunMode:  
+    once: runs once on script startup
+    default: runs every `time` seconds
+    finished: runs every `time` seconds, only if last execution has finished
+    offset: runs every `time` seconds, plus the time it the execution took
+    
+    Default = default
+
+Example `config.json`:
+```
+[
+    {
+        "name": "commandName",
+        "cmd": "echo hi >> hi.txt",
+        "time": "0.5",
+        "runMode": "default"
+    }
+]
+```
---
+++ main.py
@@ -0,0 +1,46 @@
+from subprocess import Popen, DEVNULL as NULL
+from json import load as loadJson
+from threading import Thread
+from time import sleep
+from sys import argv
+
+def run(cmd):
+    return Popen(cmd, stdout = NULL, stderr = NULL, shell = True)
+
+def runner(name, cmd, time, runMode, processes):
+    if runMode == "once":
+        processes.append(p := run(cmd))
+        p.wait()
+        processes.remove(p)
+        return
+    
+    if runMode == "default":
+        while True:
+            processes.append(p := run(cmd))
+            sleep(time)
+    
+    if runMode == "finished":
+        p = None
+        while True:
+            if p is None or (finished := (p.poll() is not None)): # start or has finished
+                if finished: processes.remove(p) # Remove from process list if it's finished
+                processes.append(p := run(cmd))
+            
+            sleep(time)
+    
+    if runMode == "offset":
+        while True:
+            processes.append(p := run(cmd))
+            p.wait()
+            processes.remove(p)
+            sleep(time)
+
+config = loadJson(open(argv[1] if len(argv) > 1 else "config.json", "r"))
+
+processes = []
+for t in [((runnerThread := Thread(target = runner, args = (
+    e["name"] if "name" in e else "default",
+    e["cmd"],
+    float(e["time"]) if "time" in e else 10,
+    e["runMode"] if "runMode" in e else "default", processes
+))), runnerThread.start()) for e in config]: t[0].join()
